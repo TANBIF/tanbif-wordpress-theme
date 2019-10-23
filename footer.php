245403ef98a93d66df87030d3b0c8178e9fdaabf
Meshack <meshackf1@gmail.com>
2019-10-23 20:41:35 +0300

    Minor Changes

--- footer.php
+++ footer.php
@@ -135,6 +135,9 @@
             <li class="footer-item footer-social">
               <a id="facebook" title="Facebook" href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/social_facebook_29px.png" alt="Facebook" /></a>
             </li>
+            <li class="footer-item footer-social">
+              <a id="facebook" title="Github Team" href="https://github.com/orgs/DevOps-2019/people"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/github.png" alt="GitHub Team" /></a>
+            </li>
             <li class="footer-item footer-item-text footer-item-start"><a href="/who-we-are/terms-of-use/#Copyright">Copyright</a></li>
             <li class="footer-item footer-item-text"><a href="/about-the-atlas/terms-of-use/">Terms of use</a></li>
             <li class="footer-item footer-item-text"><a href="/about-the-atlas/contact-us/">Contact us</a></li>
